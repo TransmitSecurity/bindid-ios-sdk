5ae0b1323f2efc4ccc76b70faa8e484bcaab794b
transmit-ops <devops@transmitsecurity.com>
2021-11-04 07:59:42 +0000

    jenkins https://jenkins.tsec-rnd.com/job/zzz_devops-testing/job/bindid-sdk-publishing/190/

---
+++ Sources/XmBindIdSDK.xcframework/ios-x86_64-simulator/XmBindIdSDK.framework/Headers/XmBindIdSDK.h
@@ -0,0 +1,21 @@
+/**
+ * \file
+ * Copyright (C) Transmit Security, LTD - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Generated using tsidlc.
+ *
+ * Package com.ts.bindid
+ */
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XmBindIdSDK.
+FOUNDATION_EXPORT double XmBindIdSDKVersionNumber;
+
+//! Project version string for XmBindIdSDK.
+FOUNDATION_EXPORT const unsigned char XmBindIdSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XmBindIdSDK/PublicHeader.h>
+
+
